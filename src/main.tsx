56563db2d9edf9d2b4da5ba53849cb4119c477a0
Anurag's Workspace <anuragsonawane2006@gmail.com>
2026-07-29 17:31:52 +0530

    fix bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
-import { createRoot, hydrateRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
 const container = document.getElementById("root")!;
-
-if (container.hasChildNodes() && container.innerHTML.trim().length > 0) {
-  hydrateRoot(container, <App />);
-} else {
-  createRoot(container).render(<App />);
-}
+createRoot(container).render(<App />);
